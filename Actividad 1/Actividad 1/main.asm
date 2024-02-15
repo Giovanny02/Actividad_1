41e29a89e683d221cfa59b64dbca51edd1fa4fa0
Giovanny02 <91983908+Giovanny02@users.noreply.github.com>
2024-02-14 21:15:26 -0600

    Update main.asm

--- Actividad 1/Actividad 1/main.asm
+++ Actividad 1/Actividad 1/main.asm
@@ -2,8 +2,8 @@
 ; Actividad 1.asm
 ;
 ; Created: 14/02/2024 12:42:03 a. m.
-; Author : Uriel Huerta, Axel Vallejo, Giovani Orozco
-;Programa para generar 4 diferentes seÒales con frecuencias distintas
+; Author : Uriel Huerta, Axel Vallejo, Giovanny Gonzalez 
+;Programa para generar 4 diferentes se√±ales con frecuencias distintas
 ;dependiendo de los estados de entrada
 
 .cseg	
@@ -25,15 +25,15 @@
 	sbi PORTB,PB4 ;activa la resistencia de pull-up en puerto 4
 	sbi PORTB,PB3 ;activa la resistencia de pull-up en puerto 3 
 ;PORTB coloca el puerto en modo escritura
-;En estos momentos ambos puertos estarÌan con un 1 lÛgico
+;En estos momentos ambos puertos estar√≠an con un 1 l√≥gico
 	
 ;Inicializar el TIMER1 de 16 bits Pagina 123 datasheet
-	ldi r17,0xFF ; Byte m·s alto de 0xFFFF
-	ldi r16,0xFF ; Byte m·s bajo de 0xFFFF
+	ldi r17,0xFF ; Byte m√°s alto de 0xFFFF
+	ldi r16,0xFF ; Byte m√°s bajo de 0xFFFF
 	out TCNT1H,R17 
 	out TCNT1L,R16
 
-;Configurar el modo de operaciÛn del Timer1
+;Configurar el modo de operaci√≥n del Timer1
 	ldi r17,0x00 ; Modo Normal
 	out TCCR1B,R17
 
@@ -41,21 +41,21 @@
 	ldi r17,0x06 ; Prescaler de 256
 	out TCCR1B,R17
 
-;Habilitar la interrupciÛn del Timer1
-	ldi r17,0x01 ; Habilitar interrupciÛn TOIE1
+;Habilitar la interrupci√≥n del Timer1
+	ldi r17,0x01 ; Habilitar interrupci√≥n TOIE1
 	out TIMSK1,R17
 
 ;Subrutinas
 entrada_00:
 	in temp,PB3
-	andi temp,0b00000000 ;verifica si est· en ceros
-	brne freq1 ;si est· en 00 salta a la frecuencia 1 
+	andi temp,0b00000000 ;verifica si est√° en ceros
+	brne freq1 ;si est√° en 00 salta a la frecuencia 1 
 	cbi PORTB,PB3
 
 entrada_11:
 	in temp,PB3
-	andi temp,0b00001100 ;verifica si est· en ceros
-	brne freq4 ;si est· en 4 salta a la frecuencia 4 
+	andi temp,0b00001100 ;verifica si est√° en ceros
+	brne freq4 ;si est√° en 4 salta a la frecuencia 4 
 	cbi PORTB,PB3
 
 freq1:
